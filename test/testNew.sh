e92e6e5ecd61367b9e986b6a5ad72a5b20b3eea7
ignaren <ignacio.arenas@alumnos.uva.es>
2020-03-12 17:35:22 +0100

    Eliminada la opcion -q de diff para ver donde estan los fallos

--- test/testNew.sh
+++ test/testNew.sh
@@ -5,11 +5,11 @@ mkdir -p testsNew
 for i in {1..20}
 do
 echo "Fase $(($i))"
-../evolution 200 25 1000 1000 1 10 $((4424+$i*127)) 6534 2461 $(($i*19059 % 4000 +1)) | sed --expression='$!d' >testsNew/Seed1Test$i; diff -q testBaseNew/Seed1Test$i testsNew/Seed1Test$i 
-../evolution 30 30 1500 1000 1 7 4624 $((9334+$i*7)) 961 10 | sed --expression='$!d' >testsNew/Seed2Test$i; diff -q testBaseNew/Seed1Test$i testsNew/Seed1Test$i
-../evolution 40 10 200 1000 1 2 4468 57764 $((2461+$i*13)) $(($i*4741 % 40 +1)) | sed --expression='$!d' >testsNew/Seed3Test$i; diff -q testBaseNew/Seed1Test$i testsNew/Seed1Test$i
-../evolution 200 10 100 1000 2 4 $((44+$i*37)) 55534 $((9361+$i*56)) 100  | sed --expression='$!d' >testsNew/Seed13Test$i; diff -q testBaseNew/Seed1Test$i testsNew/Seed1Test$i
-../evolution 5 500 75 1000 1 11 $((4324+$i*8)) $((576+$i*3)) $((461+$i*11)) $(($i*742 % 80 +1)) | sed --expression='$!d' >testsNew/Seed123Test$i; diff -q testBaseNew/Seed1Test$i testsNew/Seed1Test$i
+../evolution 200 25 1000 1000 1 10 $((4424+$i*127)) 6534 2461 $(($i*19059 % 4000 +1)) | sed --expression='$!d' >testsNew/Seed1Test$i; diff  testBaseNew/Seed1Test$i testsNew/Seed1Test$i
+../evolution 30 30 1500 1000 1 7 4624 $((9334+$i*7)) 961 10 | sed --expression='$!d' >testsNew/Seed2Test$i; diff  testBaseNew/Seed1Test$i testsNew/Seed1Test$i
+../evolution 40 10 200 1000 1 2 4468 57764 $((2461+$i*13)) $(($i*4741 % 40 +1)) | sed --expression='$!d' >testsNew/Seed3Test$i; diff  testBaseNew/Seed1Test$i testsNew/Seed1Test$i
+../evolution 200 10 100 1000 2 4 $((44+$i*37)) 55534 $((9361+$i*56)) 100  | sed --expression='$!d' >testsNew/Seed13Test$i; diff  testBaseNew/Seed1Test$i testsNew/Seed1Test$i
+../evolution 5 500 75 1000 1 11 $((4324+$i*8)) $((576+$i*3)) $((461+$i*11)) $(($i*742 % 80 +1)) | sed --expression='$!d' >testsNew/Seed123Test$i; diff  testBaseNew/Seed1Test$i testsNew/Seed1Test$i
 done
 echo "Secuencia pasada"
 # Special Cases
